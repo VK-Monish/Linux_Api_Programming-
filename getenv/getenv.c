8479490915f963f4c9bfab9ef7f90cf69ef079a1
VK-MONISH KUMAR <139607720+VK-Monish@users.noreply.github.com>
2025-05-15 12:53:07 +0530

    Getenv

--- getenv/getenv.c
+++ getenv/getenv.c
@@ -1,3 +1,37 @@
+/*
+Simple getenv() Usage in C
+
+This program demonstrates how to use the getenv() function to retrieve the value of an environment variable.
+
+---
+
+Program Behavior:
+- Defines an environment variable name ("SHELL").
+- Uses getenv() to get the value of that environment variable.
+- Prints the variable name and its value to standard output.
+
+Requirements:
+- Any operating system with standard C library support.
+- C compiler like GCC.
+
+How to Compile:
+    gcc getenv.c -o getenv
+
+How to Run:
+    ./getenv
+
+Expected Output:
+    SHELL :: /bin/bash
+
+(Note: The output value depends on the current environment variable settings.)
+
+Notes:
+- getenv() returns NULL if the environment variable does not exist.
+- This example assumes "SHELL" is defined in your environment.
+- Always check for NULL return in real applications for robustness.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
