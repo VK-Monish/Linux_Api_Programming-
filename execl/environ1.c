40a23bad59f5a6a34fa9edc3f49801e9df44c7a0
VK-MONISH KUMAR <139607720+VK-Monish@users.noreply.github.com>
2025-05-23 18:54:38 +0530

    excel

--- execl/environ1.c
+++ execl/environ1.c
@@ -1,3 +1,33 @@
+/*
+ * File: exec_env_example.c
+ * Description:
+ *   This program demonstrates how to use fork and execvpe system calls
+ *   to execute a new program with a custom environment from a child process.
+ *
+ * Usage:
+ *   1. Compile the program: gcc exec_env_example.c -o exec_env_example
+ *   2. Ensure that '/home/moni/bin/a.out' exists and is executable.
+ *      This is the program that will be executed by the child process.
+ *   3. Run the program: ./exec_env_example
+ *
+ * Program Behavior:
+ *   - The parent process forks a child.
+ *   - The child process executes the program "a.out" using execvpe,
+ *     with a custom environment variable "MY_PATH=1234".
+ *   - The parent process prints its PID and terminates.
+ *
+ * Example Output:
+ *   Parent PID: 4321
+ *   Child Process: MY_PATH = 1234   (Printed by a.out if it uses getenv)
+ *
+ * Notes:
+ *   - The environment variable MY_PATH is set only for the child process.
+ *   - The child executes /home/moni/bin/a.out, so make sure this path is correct.
+ *   - execvpe is a GNU extension and might not be available on all systems.
+ *
+ * Author: Monish Kumar
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
