1e09aa81ea2c6574b17328e32ae962112e254b49
VK-MONISH KUMAR <139607720+VK-Monish@users.noreply.github.com>
2025-05-26 16:25:46 +0530

    setuid

--- setuid/setuid.c
+++ setuid/setuid.c
@@ -1,3 +1,19 @@
+/*
+ * This program demonstrates how to change the effective user ID (UID) of a process
+ * using the `setuid()` system call and retrieve user account information using `getpwuid()`.
+ *
+ * Key steps:
+ * - It first prints the current real UID using `getuid()`.
+ * - Then attempts to change the UID to 0 (root) using `setuid(0)`.
+ * - If the operation fails, it prints an error using `perror()`.
+ * - After attempting the UID change, it again prints the current UID.
+ * - Finally, it uses `getpwuid()` to retrieve and print the username associated with the current UID.
+ *
+ * Note:
+ * - To successfully set UID to 0, the program must be run with elevated privileges (e.g., as root or setuid root binary).
+ * - Running without proper permissions will result in a "setuid: Operation not permitted" error.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
