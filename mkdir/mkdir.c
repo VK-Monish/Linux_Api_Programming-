8fc6e5b8588ba8cbd1914827d3311b39e337ce28
VK-MONISH KUMAR <139607720+VK-Monish@users.noreply.github.com>
2025-05-27 15:44:40 +0530

    mkdir

--- mkdir/mkdir.c
+++ mkdir/mkdir.c
@@ -1,3 +1,26 @@
+/*
+ * File: mkdir_example.c
+ * Description:
+ *   This program demonstrates how to create a new directory in C using the `mkdir()` system call
+ *   from <sys/stat.h>. It attempts to create a directory named "hello" with permission 0766.
+ *
+ * Usage:
+ *   Compile: gcc mkdir_example.c -o mkdir_example
+ *   Run: ./mkdir_example
+ *
+ * Behavior:
+ *   - If the directory "hello" does not already exist, it will be created with read, write, and execute
+ *     permissions for the owner, and read and write permissions for group and others.
+ *   - If the directory already exists or creation fails for another reason, an error message will be displayed
+ *     using perror().
+ *
+ * Note:
+ *   The permissions `0766` correspond to:
+ *     - Owner: read, write, execute (7)
+ *     - Group: read, write (6)
+ *     - Others: read, write (6)
+ */
+
 #include <stdio.h>
 #include <sys/stat.h>
 
